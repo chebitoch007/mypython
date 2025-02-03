a2ec650701a6991d321cc6ca144e90d8aceaa46c
chebitoch007 <eliphazchebitoch@gmail.com>
2025-02-03 13:13:23 +0300

    python commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to display
+
+print("Chebitoch")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ variable.py
@@ -0,0 +1,18 @@
+#A variable is a container used to store a value
+
+number = 10
+firstname = "Eliphaz"
+lastname = "Chebitoch"
+language = "Python"
+a = 10
+b = 15
+
+print(number)
+print(firstname)
+print(lastname)
+print(language ,"is a web programming language")
+print(a+b)
+
+#The sum of 10 and 15 is 25
+print(a+b)
+
